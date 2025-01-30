d1ce110e2a2060f28f781bddd1a2313a36c17bbe
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-01-30 20:20:02 +0500

    Updated header section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import Body from './body'
+import Section1 from './Section1'
 const App = () => {
   return (
+    <>
     <Header/>
+   <Body></Body>
+   <Section1></Section1>
+   </>
   )
 }
 
